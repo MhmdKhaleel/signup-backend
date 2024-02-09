36f466a68f032b18b5373827487a5dafeef29b1a
MhmdKhaleel <khaleel3936@gmail.com>
2024-02-09 12:19:05 +0530

    msg

--- controller/signuprouter.js
+++ controller/signuprouter.js
@@ -1,26 +1,32 @@
-const express=require("express")
-const signupmodel=require("../model/signupmodel")
-const router=express.Router()
-const bcrypt=require("bcryptjs")
+const express = require("express")
+const signupmodel = require("../model/signupmodel")
+const router = express.Router()
+const bcrypt = require("bcryptjs")
 
-hashPassGen=async(pass)=>{
-    const salt= await bcrypt.genSalt(10)
-    return bcrypt.hash(pass,salt)
+hashPassGen = async (pass) => {
+    const salt = await bcrypt.genSalt(10)
+    return bcrypt.hash(pass, salt)
 }
 
-router.post('/signup',async(req,res)=>{
-    let {data}={"data":req.body}
-    let password=data.password
+router.post('/signup', async (req, res) => {
+    let { data } = { "data": req.body }
+    let password = data.password
     hashPassGen(password).then(
-        (hashPass)=>{
-            console.log(hashPass)
+        (hashPass) => {
+
+            data.password = hashPass
+
+            let signup = new signupmodel(data)
+            let result = signup.save()
+            res.json({ status: "success" })
+            console.log(data)
         }
     )
-
-    console.log(data)
-    // let signup=new signupmodel(data)
-    // let result=await signup.save()
-    res.json({status:"success"})
 })
 
-module.exports=router
+router.post('/signin',(req,res)=>{
+    let input=req.body
+    
+})
+
+module.exports = router
